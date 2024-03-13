e36a0bd1e70453f28def46d14f32d13e3d1e3d90
Matteo0421 <matteo.carola99@gmail.com>
2024-03-13 14:47:31 +0100

    [add]index

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue;
+
+// scrivere tutto il codice dentro a createapp
+createApp({
+
+  // scrivere tutte le nostre variabili dentro Data
+  data(){
+    return{
+
+    }
+  },
+
+  // si scrivono tutte le funzioni
+  methods:{
+
+    // nomefunzione(){
+    //   corpo funzione
+    // }
+  }
+
+}).mount('#app');
